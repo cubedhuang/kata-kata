a1b6bb470ec8196c06343fdce51e00132b6f4a79
cubedhuang <99danielh@gmail.com>
2023-02-14 23:51:09 -0500

    tabel

--- src/routes/aka-nemune/Creators.svelte
+++ src/routes/aka-nemune/Creators.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { Word } from '$lib/types';
+
 	import Separator from '$lib/components/Separator.svelte';
+	import Table from './Table.svelte';
 
 	export let words: Word[];
 
@@ -24,29 +26,4 @@
 </script>
 
 <Separator>creators (some overlap)</Separator>
-
-<table class="w-full">
-	{#each sortedNames as [name, words]}
-		<tr class="border-b last:border-0">
-			<th class="w-32 py-1 font-bold text-left align-text-top">
-				{name}
-			</th>
-
-			<td class="pr-2 py-1 text-right align-text-top faded">
-				{words.length}
-			</td>
-
-			<td class="pl-2 py-1 border-l">
-				<ul class="flex flex-wrap gap-x-2">
-					{#each words as word}
-						<li>
-							<a href="/{word}" class="text-blue-500">
-								{word}
-							</a>
-						</li>
-					{/each}
-				</ul>
-			</td>
-		</tr>
-	{/each}
-</table>
+<Table items={sortedNames} />
---
+++ src/routes/aka-nemune/Table.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let items: [string, string[]][];
+</script>
+
+<table class="w-full">
+	{#each items as [title, words]}
+		<tr class="border-b last:border-0">
+			<th class="py-1 font-bold text-left align-text-top">
+				{title}
+			</th>
+
+			<td class="pr-2 py-1 text-right align-text-top faded">
+				{words.length}
+			</td>
+
+			<td class="pl-2 py-1 border-l align-text-top">
+				<ul class="flex flex-wrap gap-x-2">
+					{#each words as word}
+						<li>
+							<a href="/{word}" class="text-blue-500">
+								{word}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</td>
+		</tr>
+	{/each}
+</table>
